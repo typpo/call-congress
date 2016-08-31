ce5d78fc7882b569b0c7bd9a6cddf179f845bf9c
Ian Webster <ianw_git@ianww.com>
2016-08-30 22:09:53 -0700

    remove comment

--- controllers/phone.js
+++ controllers/phone.js
@@ -1,35 +1,6 @@
 var twilio = require('twilio');
 var request =  require('request');
 
-// Example response:
-/*
-{ Called: '+16504698114',
-  ToState: 'CA',
-  CallerCountry: 'US',
-  Direction: 'inbound',
-  CallerState: 'NY',
-  ToZip: '94022',
-  CallSid: 'CA6876c31ff3f3aa70edc513161a73b53b',
-  To: '+16504698114',
-  CallerZip: '10579',
-  ToCountry: 'US',
-  ApiVersion: '2010-04-01',
-  CalledZip: '94022',
-  CalledCity: 'LOS ALTOS',
-  CallStatus: 'ringing',
-  From: '+19148741159',
-  AccountSid: 'AC7b5178b4fe2c349a8fa476ccb6c51e25',
-  CalledCountry: 'US',
-  CallerCity: 'WHITE PLAINS',
-  Caller: '+19148741159',
-  FromCountry: 'US',
-  ToCity: 'LOS ALTOS',
-  FromCity: 'WHITE PLAINS',
-  CalledState: 'CA',
-  FromZip: '10579',
-  FromState: 'NY' }
-*/
-
 var CONGRESS_API_URL = 'https://congress.api.sunlightfoundation.com/legislators/locate?apikey=' +
     process.env.SUNLIGHT_FOUNDATION_KEY
 
