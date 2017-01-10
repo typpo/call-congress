3264893a4b67d29764a96dd5cfacb323fd031600
Andrew Davis <andrew@gigantic.computer>
2017-01-09 22:44:56 -0600

    Reduce test boilerplate by using supertest
    * tests no longer require a port for running the tests
    * see https://github.com/visionmedia/superagent for supertest api docs

--- test/phone.js
+++ test/phone.js
@@ -2,61 +2,45 @@
 
 const assert = require('assert');
 const path = require('path');
-const request = require('request');
-const debug = require('debug')('congress:test:phone');
+const request = require('supertest');
 
-require('dotenv').config({
-  path: path.join(__dirname, '../.env'),
-});
-
-// TODO(ian): Add a way to properly pass in a config.
 const app = require('../cyc_entry.js');
 
-let server;
-
-const PORT = 8082;
-const URL = `http://localhost:${PORT}`;
-
 describe('phone', () => {
-  beforeEach((done) => {
-    server = app.listen(PORT, done);
-  });
-
-  afterEach((done) => {
-    server.close(done);
-  });
-
   it('new call', (done) => {
-    request.post(`${URL}/new_phone_call`, (err, res, body) => {
-      if (err) return done(err);
-      assert.equal(res.statusCode, 200);
-      assert.notEqual(
-        body.indexOf('audio/v2/zip_prompt.mp3'), -1,
-        '/new_phone_call should play audio/v2/zip_prompt.mp3');
-      return done();
-    });
+    request(app)
+      .post(`/new_phone_call`)
+      .expect(200)
+      .expect((res) => {
+        assert.notEqual(
+          res.text.indexOf('audio/v2/zip_prompt.mp3'), -1,
+          '/new_phone_call should play audio/v2/zip_prompt.mp3');
+      })
+      .end(done)
   });
 
   describe('redirect call', () => {
     it('enforces zip code', (done) => {
-      request.post(`${URL}/redir_call_for_zip`, (err, res, body) => {
-        if (err) return done(err);
-        debug(body);
-        assert.notEqual(
-          body.indexOf('audio/v2/error.mp3'), -1,
-          '/redir_call_for_zip should reject callers without a zip code');
-        return done();
-      });
+      request(app)
+        .post(`/redir_call_for_zip`)
+        .expect(200)
+        .expect((res) => {
+          assert.notEqual(
+            res.text.indexOf('audio/v2/error.mp3'), -1,
+            '/redir_call_for_zip should reject callers without a zip code');
+        })
+        .end(done);
     });
 
     it('looks up senators', (done) => {
-      request.post(`${URL}/redir_call_for_zip`, { form: { Digits: '10583' } }, (err, res, body) => {
-        if (err) return done(err);
-        debug(body);
-        assert(body.indexOf('audio/v2/senator.mp3') > -1,
-               'Response contains a senator recording');
-        return done();
-      });
+      request(app)
+        .post(`/redir_call_for_zip`)
+        .send({ Digits: '10583' })
+        .expect((res) => {
+          assert(res.text.indexOf('audio/v2/senator.mp3') > -1,
+                 'Response contains a senator recording');
+        })
+        .end(done);
     });
 
     // TODO(thosakwe): Add a test for auto-finding the zip code of a repeat caller
