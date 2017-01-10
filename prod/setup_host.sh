925393ac6ecc0ef51af04ebdf77b81632fe04582
Ian Webster <ianw_github@ianww.com>
2017-01-09 21:12:04 -0800

    Add prod host setup script

---
+++ prod/setup_host.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# Setup on Ubuntu or debian machines
+
+pushd ~
+
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get -y install nginx mosh unattended-upgrades git-core nodejs
+
+npm install -g yarn pm2
+
+git clone https://github.com/StayWokeOrg/general-congress-hotline.git
+cd general-congress-hotline
+
+yarn install
+
+popd
+
+echo "Done."
