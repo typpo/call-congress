0843b87aca13e2c259256aec61e0d51e81be61bb
Ian Webster <ianw_git@ianww.com>
2017-01-13 10:01:28 -0800

    Move washington dc special case constants to their own file

---
+++ controllers/dc.js
@@ -0,0 +1,10 @@
+// Constants for Washington, D.C. special case.
+
+const zipCodes = [20001, 20002, 20003, 20004, 20005, 20006, 20007, 20008, 20009, 20010, 20011, 20012, 20015, 20016, 20017, 20018, 20019, 20020, 20024, 20032, 20036, 20037, 20045, 20052, 20053, 20057, 20064, 20202, 20204, 20228, 20230, 20240, 20245, 20260, 20307, 20317, 20319, 20373, 20390, 20405, 20418, 20427, 20506, 20510, 20520, 20535, 20540, 20551, 20553, 20560, 20565, 20566, 20593];
+
+const paulRyanObj = {"bioguide_id":"R000570","birthday":"1970-01-29","chamber":"senate","contact_form":null,"crp_id":"N00004357","district":1,"facebook_id":"924244934290700","fax":null,"fec_ids":["H8WI01024"],"first_name":"Paul","gender":"M","govtrack_id":"400351","icpsr_id":29939,"in_office":true,"last_name":"Ryan","leadership_role":"Speaker","middle_name":"D.","name_suffix":null,"nickname":null,"oc_email":"Rep.Paulryan@opencongress.org","ocd_id":"ocd-division/country:us/state:wi/cd:1","office":"1233 LongworthHouse Office Building","party":"R","phone":"202-225-3031","state":"WI","state_name":"Wisconsin","term_end":"2019-01-03","term_start":"2017-01-03","thomas_id":"01560","title":"Rep","twitter_id":"SpeakerRyan","votesmart_id":26344,"website":"http://paulryan.house.gov","youtube_id":"reppaulryan"};
+
+module.exports = {
+  zipCodes,
+  paulRyanObj,
+};
