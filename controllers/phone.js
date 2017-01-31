cc23426a772c6bbad066811e02531e04f2612bd9
Ian Webster <ianw_git@ianww.com>
2017-01-31 08:08:34 -0800

    call.message --> call.sms

--- controllers/phone.js
+++ controllers/phone.js
@@ -117,7 +117,7 @@ function callPeople(people, res) {
       phoneCall(call, phone);
     });
     call.play(config.audio.done);
-    call.message('Thanks for your support. Reply "yes" to opt-in to occasional notifications for other resistance opportunities.');
+    call.sms('Thanks for your support. Reply "yes" to opt-in to occasional notifications for other resistance opportunities.');
   }
 
   res.type('text/xml');
