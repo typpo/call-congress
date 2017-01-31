5e1f2fe7300411c843c0256b9b066e9e9f3eb0e3
Ian Webster <ianw_git@ianww.com>
2017-01-31 08:14:23 -0800

    read text response

--- controllers/sms.js
+++ controllers/sms.js
@@ -2,7 +2,13 @@ const twilio = require('twilio');
 
 function receivedText(req, res) {
   const twiml = new twilio.TwimlResponse();
-  twiml.message('Thank you!');
+
+  const response = req.body.Body.toLowerCase();
+  if (response.indexOf('yes') > -1) {
+    twiml.message('Thank you!');
+  } else {
+    twiml.message('To opt-in to notifications, reply with "yes"');
+  }
 
   res.status(200);
   res.type('text/xml');
