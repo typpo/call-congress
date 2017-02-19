616eb5043df7587c04a68c02d1aac8af5077a915
Ian Webster <ianw_git@ianww.com>
2017-02-18 16:44:46 -0800

    error redirect: go up one (to support relative hosting)

--- controllers/error-redirect.js
+++ controllers/error-redirect.js
@@ -9,7 +9,7 @@ function errorRedirect(req, res) {
   const call = new twilio.TwimlResponse();
   // TODO(diffalot): use a recording instead of TTS
   call.say({ voice: 'woman'}, 'We did not understand your input, please try again')
-  call.redirect(`/${redirect}`);
+  call.redirect(`../${redirect}`);
   res.type('text/xml');
   res.status(200);
   res.send(call.toString());
