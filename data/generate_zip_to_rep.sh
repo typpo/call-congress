57e260abc0573757786c1608a9e968c05a537687
Ian Webster <ianw_github@ianww.com>
2017-12-13 15:42:42 +0000

    use HUD zip codes to add mappings for some missing reps

--- data/generate_zip_to_rep.sh
+++ data/generate_zip_to_rep.sh
@@ -5,6 +5,7 @@ pushd `dirname $0` &>/dev/null
 mkdir -p raw
 
 wget -O raw/zip_to_cd.csv 'https://raw.githubusercontent.com/OpenSourceActivismTech/us_zipcodes_congress/master/zccd.csv'
+wget -O raw/zip_to_cd_hud.csv 'https://raw.githubusercontent.com/OpenSourceActivismTech/us_zipcodes_congress/master/zccd_hud.csv'
 wget -O raw/legislators.csv 'https://theunitedstates.io/congress-legislators/legislators-current.csv'
 python merge.py
 
